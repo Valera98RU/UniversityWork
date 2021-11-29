5531e34cbbd1354997cee8b5e0308e647c6b1f27
TimmyYang1 <Timmyyangprod@gmail.com>
2021-11-29 23:54:31 +0700

    Переработак паттерн и добавлены комментарии

--- Decorator.php
+++ Decorator.php
@@ -2,111 +2,150 @@
 
 namespace Decorator\Main;
 
-
+/**
+ * Библиотека работы с фото файлами
+ */
 use GdImage;
 
+/**
+ * Глобальные константы с названием фото фильтров
+ */
 define('NEGATIVE_FILTER', 'Negative');
 define('MEAN_REMOVAL', 'MeanRemoval');
 define('GAUSSIAN', 'Gaussian');
 
+/**
+ * Интерфейс фото фильтров
+ */
 interface IFilter
 {
+    /**
+     * Применяет фильтр к изображению
+     */
     public function ApplyFilter(GdImage &$image): void;
 }
-
+/**
+ * Базовый декоратов фильтров
+ */
 class BaseFilterDecorator implements IFilter
 {
-    private IFilter $filter;
+    protected IFilter $filter;
 
     public function __construct(IFilter $filter)
     {
-        $this->filter = $filter;
+        $this->filter = new BaseFilter();
     }
 
-    public function ApplyFilter(GdImage &$image): void
-    {
-        $this->filter->ApplyFilter($image);
-    }
+    public abstract function ApplyFilter(GdImage &$image);    
 }
 
-class Negative implements IFilter
+/**
+ * Базой фильтр
+ */
+class BaseFilter implements IFilter
 {
-    public function ApplyFilter(GdImage &$image): void
+    private string $filterType = "";
+    private array $param = null;
+
+
+    public function setFilterType(string $type):void
     {
-        imagefilter($image, IMG_FILTER_NEGATE, [IMG_FILTER_PIXELATE => 512]);
+        $this->$filterType = $type;
+    }
+    public function setFilterParams(array $param):void
+    {
+        $this->$param = $param;
+    }
+    public function ApplyFilter(GdImage &$image):variant_mod
+    {
+        if($filterType === "")
+        {
+            throw new Excption("Тип фильтра не задан");
+        }        
+        imagefilter($image, $filterType, $param);
     }
 }
 
-class MeanRemoval implements IFilter
-{
-    public function ApplyFilter(GdImage &$image): void
-    {
-        imagefilter($image, IMG_FILTER_MEAN_REMOVAL, [IMG_FILTER_PIXELATE => 512]);
-    }
-}
 
-class Gaussian implements IFilter
-{
-    public function ApplyFilter(GdImage &$image): void
-    {
-        imagefilter($image, IMG_FILTER_GAUSSIAN_BLUR, [IMG_FILTER_PIXELATE => 512]);
-    }
-}
 
+/**
+ * Фичего не делает
+ */
 class Nothing implements IFilter
 {
     public function ApplyFilter(GdImage &$image): void
     {
-
+        //ничего не делаем
     }
 }
-
+/**
+ * Декоратов негативного фильтра
+ */
 class NegativeDecorator extends BaseFilterDecorator
 {
     public function ApplyFilter(GdImage &$image): void
-    {
-        parent::ApplyFilter($image);
-        $filter = new Negative();
-        $filter->ApplyFilter($image);
+    {               
+        //настраиваем  фильтр негатива     
+        $this->filter->setFilterType(IMG_FILTER_NEGATE);
+        $this->filter->setFilterParams([IMG_FILTER_PIXELATE => 512]);
+        //применяем фильтр из библиотеки с статическими настройками
+        $this->filter->ApplyFilter($image);
+       
     }
 }
-
+/**
+ * Декоратор фильтра для достижения эффекта эскиза
+ */
 class MeanRemovalDecorator extends BaseFilterDecorator
 {
     public function ApplyFilter(GdImage &$image): void
-    {
-        parent::ApplyFilter($image);
-        $filter = new MeanRemoval();
-        $filter->ApplyFilter($image);
+    {     
+        //Настраиваем фильтр эффекта эскиза
+        $this->$filter->setFilterType(IMG_FILTER_MEAN_REMOVAL);
+        $this->filter->setFilterParams([IMG_FILTER_PIXELATE => 512]);
+        //применяем фильтр из библиотеки с статическими настройками
+        $this->filter->ApplyFilter($image);
     }
 }
-
+/**
+ * Декоратор фильтра размытия по гаусу
+ */
 class GaussianDecorator extends BaseFilterDecorator
 {
     public function ApplyFilter(GdImage &$image): void
-    {
-        parent::ApplyFilter($image);
-        $filter = new Gaussian();
-        $filter->ApplyFilter($image);
+    {     
+        //Настраиваем фильтр размытия по гаусу
+        $this->filter->setFilterType(IMG_FILTER_GAUSSIAN_BLUR);
+        $this->filter->setFilterParams([IMG_FILTER_PIXELATE => 512]);
+        //применяем фильтр из библиотеки с статическими настройками
+        $this->filter->ApplyFilter($image);
     }
 }
 
-
+/**
+ * Выполяемый код
+ */
 class main
 {
     public function do(string $path, array $filters)
     {
+        //Инициализируем объект фотографии
         $images = ImageCreateFromJPEG($path);
+        //Иннициализируем фильтр по умолчанию
         $filter = new Nothing();
         if (in_array(NEGATIVE_FILTER, $filters)) {
-            $filter = new NegativeDecorator($filter);
+            //если передан фильтр негатива то переинициализируем фильтр
+            $filter = new NegativeDecorator();
         }
         if (in_array(MEAN_REMOVAL, $filters)) {
-            $filter = new MeanRemovalDecorator($filter);
+            // если передан фильтр эффекта эскиза то переинициализируем фильтр
+            $filter = new MeanRemovalDecorator();
         }
         if (in_array(GAUSSIAN, $filters)) {
-            $filter = new GaussianDecorator($filter);
+             // если передан фильтр разытия по гаусу то переинициализируем фильтр
+            $filter = new GaussianDecorator();
         }
+        //применяем фильтр
         $filter->ApplyFilter($images);
     }
 }
